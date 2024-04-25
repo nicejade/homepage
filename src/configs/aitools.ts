766f5dc224d24aaa24570e5ed7f5a923a6c27186
杨琼璞 <jeffygisgreat@gmail.com>
2024-04-25 13:36:15 +0800

    :memo: New add AI module content

---
+++ src/configs/aitools.ts
@@ -0,0 +1,16 @@
+export default {
+  title: '人工智能',
+  style: 'default',
+  list: [
+    {
+      title: 'OpenAI | 一家人工智能研究和部署公司',
+      link: 'https://openai.com/',
+      desc: 'OpenAI 是一家人工智能研究和部署公司。其使命是确保通用人工智能造福全人类；旗下推出 ChatGPT、GPT-4、Sora、DALL-E 等自然语言生成式模型，风靡全球；OpenAI：引领人工智能革命的领军者。'
+    },
+    {
+      title: 'Ollama | 启动并运行大型语言模型',
+      link: 'https://ollama.com/',
+      desc: 'Ollama 是一个提供大型语言模型服务的平台，它允许用户运行和定制多种先进的语言模型，包括Llama3、Phi3 和 Gemma等。这些模型通常用于执行复杂的自然语言处理任务，如文本生成、语言翻译、问答系统和其他多种智能文本分析功能。'
+    },
+  ]
+}
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,11 @@
 import projects from "./projects"
 import services from "./services"
+import aitools from "./aitools"
 import softwares from "./softwares"
 
 export default [
   projects,
   services,
+  aitools,
   softwares,
 ]
