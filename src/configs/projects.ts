ae63b9a13b45afef582fc919c517c2b014b77fdb
杨琼璞 <jeffygisgreat@gmail.com>
2024-04-26 13:45:38 +0800

    :wrench: Update links config

--- src/configs/projects.ts
+++ src/configs/projects.ts
@@ -3,32 +3,32 @@ export default {
   style: 'stagger',
   list: [
     {
-      name: '晚晴幽草轩',
+      title: '晚晴幽草轩',
       link: 'https://www.jeffjade.com/',
       desc: '基于 Hexo 所构建、部署于 Github Pages 服务的个人博客；名字取自：“天意怜幽草，人间重晚晴”。专注分享互联网应用（Web 应用、快应用、小程序等）开发，以及优质网站、AI 应用、效率工具、心得感悟等内容。',
     },
     {
-      name: '缘知随心庭',
+      title: '缘知随心庭',
       link: 'https://fine.niceshare.site/',
       desc: '缘知随心庭，基于 Astro、Svelte 所构建的个人知识管理与分享博客，部署于 Cloudflare 内容分发网络；主要用于记录学习笔记、技术分享、生活感悟等。',
     },
     {
-      name: '悠然宜想亭',
+      title: '悠然宜想亭',
       link: 'https://forum.lovejade.cn/',
       desc: '基于 Flarum 所搭建的现代化社区，旨在营造温润、宽厚、互助的现代化社区；您可以在这里探讨技术、寻求创意、分享美食、聊聊工作、发布科普等等，甚至是为喜欢的作品打 Call、推广，只要遵纪守法、恪守道德，此隅云间，便是您悠然交流、适宜分享之所。',
     },
     {
-      name: '玉桃文飨轩',
+      title: '玉桃文飨轩',
       link: 'https://share.lovejade.cn/',
       desc: '玉桃文飨轩🍑 ，简单好用的在线文本工具；支持将 Markdown、Rich Text 等格式内容，快速转化为 png、pdf、html 等文件，并支持自定义设置、一键下载等功能。基于 Vue3 、 Vite 、 Pinia 、 TailwindCSS 、 TypeScript 构建。',
     },
     {
-      name: 'Arya - 在线 Markdown 编辑器',
+      title: 'Arya - 在线 Markdown 编辑器',
       link: 'https://markdown.lovejade.cn/',
       desc: 'Arya，是一款基于 Vue、Vditor，为未来而构建的在线 Markdown 编辑器；轻量且强大：内置粘贴 HTML 自动转换为 Markdown，支持流程图、甘特图、时序图、任务列表，可导出携带样式的图片、PDF、微信公众号特制的 HTML 等。',
     },
     {
-      name: '为高效而生：Arya Jarvis',
+      title: '为高效而生：Arya Jarvis',
       link: 'https://arya.lovejade.cn/',
       desc: 'Arya Jarvis 是一款基于 Node.js 所编写命令行工具，旨在为开发人员节省更多时间和精力（譬如：用于快速构建本地 Web 服务器、一键找到您本地 IP 地址并复制到剪切板、为指定文本生成二维码等）；适用于 Mac、Windows、Linux 等平台。',
     }
--- src/configs/softwares.ts
+++ src/configs/softwares.ts
@@ -3,34 +3,44 @@ export default {
   style: 'default',
   list: [
     {
-      name: 'Raycast - Supercharged productivity',
+      title: 'Raycast - Supercharged productivity',
       link: 'https://www.raycast.com/',
       desc: 'Raycast是一款功能强大的工作流助手软件，旨在提升用户在电脑上的效率和生产力。它为用户提供了快速访问和执行各种任务的便捷方式。',
     },
     {
-      name: 'Espanso | 一个隐私优先的跨平台文本扩展器',
+      title: 'Espanso | 一个隐私优先的跨平台文本扩展器',
       link: 'https://espanso.org/',
       desc: 'Espanso 是一个用 Rust 编写的跨平台文本扩展器（隐私优先），它可以检测您何时键入特定的关键字，并用其他内容替换它，能辅助节省大量打字时间。',
     },
     {
-      name: 'Obsidian ｜ 锐化你的思维',
+      title: 'Obsidian ｜ 锐化你的思维',
       link: 'https://obsidian.md/',
       desc: 'Obsidian 是一个强大的知识库、一款好用的笔记应用程序，位于纯文本 Markdown 文件的本地文件夹之上。其目标是：永远成为你第二个大脑。',
     },
     {
-      name: 'Shottr ｜ 专为 MacOS 的截图注释应用程序',
+      title: 'Shottr ｜ 专为 MacOS 的截图注释应用程序',
       link: 'https://shottr.cc/',
       desc: 'Shottr 是一个 免费 的 MacOS 屏幕截图应用程序，具有滚动屏幕截图、OCR、注释和测量工具；具有小巧、快速、人性化等特点，专为那些关心像素的人打造。',
     },
     {
-      name: 'Kap ｜ 捕捉你的屏幕',
+      title: 'Kap ｜ 捕捉你的屏幕',
       link: 'https://getkap.co/',
       desc: 'Kap 是一款免费且开源的 Mac 录屏软件（使用 Web 技术构建的开源屏幕录像机），它支持多种录制方式，自带视频格式编辑工具，质量更高，噪音更低。',
     },
     {
-      name: 'Xmind | 思维导图应用',
+      title: 'Xmind | 思维导图应用',
       link: 'https://xmind.app/',
       desc: 'XMind 是一个全功能的思维导图和头脑风暴软件，为激发灵感和创意而生。作为一款有效提升工作和生活效率的生产力工具，受到全球百千万用户的青睐。',
+    },
+    {
+      title: 'Trello | 将您的所有任务、队友和工具整合在一起',
+      link: 'https://trello.com/',
+      desc: 'Trello 是一种简便、免费、灵活、免费、可视化、高扩展的任务管理器；其看板、列表和卡片使你能够以一种有趣、灵活和有益的方式，组织你的项目并划分它们的优先顺序。',
+    },
+    {
+      title: 'Docker | 在任何地方构建、发布和运行任何应用',
+      link: 'https://www.docker.com/',
+      desc: 'Docker 是一种开源软件，可以将任何应用程序打包，运送和运行的轻量级容器；她适用于所有应用的现代平台，她使得你可以按照自己的方式，随处构建，管理和保护您的应用程序，且无需考虑更多。',
     }
   ],
 }
