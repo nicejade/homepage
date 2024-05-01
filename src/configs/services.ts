73ddda3568daf4cabd63e49618056f63d5fa6b98
杨琼璞 <jeffygisgreat@gmail.com>
2024-05-01 23:29:28 +0800

    :wrench: Update config | services & selfhosted

--- src/configs/services.ts
+++ src/configs/services.ts
@@ -8,9 +8,39 @@ export default {
       desc: 'Flaticon 是最大的免费、矢量图标数据库，用于个人和商业用途；它提供 PNG，SVG，EPS，PSD，和 CSS 格式；同时，Flaticon 也是世界上最大的免费图标搜索引擎，你可以在这里检测、下载各种你想要的图标和贴纸。'
     },
     {
+      title: 'Tabler Icons | 高度可定制的免费 SVG 图标',
+      link: 'https://www.flaticon.com/',
+      desc: 'Tabler Icons，提供 4926 个开源免费 SVG 图标，高度可定制，无需归属，可用于商业用途。可您在 Web 项目中直接使用，您可以在线设置图标的尺寸、Stroke、以及颜色；而且还提供内置搜索功能，整体体验良好。'
+    },
+    {
       title: 'iLoveIMG | 在线图片编辑器',
       link: 'https://www.iloveimg.com/',
       desc: 'iLoveIMG 是一款 Web 应用，可让您在几秒钟内免费修改图像；这里有您想要批量编辑图片的所有工具，只需点击几下，即可根据自己的需要，对图片进行批量压缩、裁剪、格式转换或尺寸调整，还可以同时处理不同的图片文件。',
+    },
+    {
+      title: 'iLovePDF | PDF 爱好者的在线工具',
+      link: 'https://tinypng.com/',
+      desc: 'iLovePDF 是一个完全免费且易于使用的 PDF 文件的在线服务。使用 PDF 所需的所有工具都触手可及。全部都是 100% 免费且易于使用！只需点击几下即可合并、分割、压缩、转换、旋转、解锁 PDF 并为其添加水印。',
+    },
+    {
+      title: 'TinyPNG | 智能压缩 WebP、PNG 和 JPEG',
+      link: 'https://tinypng.com/',
+      desc: 'TinyPNG 使用智能有损压缩技术，将您的 WebP、PNG 和 JPEG 等格式文件大小降低。 通过选择性的减少图片中的颜色，只需要很少的字节数就能保存数据。 对视觉的影响几乎不可见，但是在文件大小上有非常大的差别。',
+    },
+    {
+      title: 'TinySnap | 所有人可用的截图扩展工具',
+      link: 'https://tinysnap.app/',
+      desc: 'TinySnap 是为每个人，特别是创造者、开发者和营销者，而创建的可生产的屏幕截图扩展（Chrome 浏览器）工具，完全免费可用；您不需要花哨的设计技能，只需点击几下，即可将无聊的屏幕截图变成引人注目的视觉效果。',
+    },
+    {
+      title: 'Shots | 创建惊人的模型（Mockups）',
+      link: 'https://shots.so/',
+      desc: 'Shots，很精致的一款 Mockups 工具，可用来创建令人惊叹的设备模型，做出非常漂亮的图片效果；它支持多类模型，如 iPhone、Mac、iMac、iWatch、截图类等场景，且支持修改如图片弧度、3D、偏移量、背景、比例等。',
+    },
+    {
+      title: 'Device Shots | 社交平台的设备模型',
+      link: 'https://deviceshots.com/',
+      desc: 'Device Shots，一款超棒的样机生成网站；它能在几秒钟内即可创建不同设备的样机，通过网站或移动应用程序的屏幕截图，免费获取高分辨率的设备模型，比如：手机、电脑、手表等等；它不仅访问速度超快，而且支持超多设备。',
     },
   ]
 }
