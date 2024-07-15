052a74747f32e2b76ae5c9f8aaf69864abc21ceb
杨琼璞 <jeffygisgreat@gmail.com>
2024-07-15 13:49:19 +0800

    ✨ Update links config

--- src/configs/coding.ts
+++ src/configs/coding.ts
@@ -12,5 +12,10 @@ export default {
       link: 'https://github.com/features/copilot',
       desc: 'GitHub Copilot 是由 GitHub 与 OpenAI 合作开发的编程助手工具，基于先进的 AI 技术。它能够根据用户输入和上下文提供智能代码建议和自动补全，支持多种编程语言。GitHub Copilot 不仅可以加速编码过程，还能提高代码质量和生产效率。这款工具集成在多种开发环境中，帮助开发者快速创建和编辑代码。'
     },
+    {
+      title: 'Prettier | 固执己见的代码格式化程序',
+      link: 'https://prettier.io/',
+      desc: 'Prettier 是一款流行的代码格式化工具，支持多种编程语言，及多款代码编译器。它能自动调整代码的缩进、空格、分号等风格，确保代码风格的一致性和可读性。Prettier 简化了代码格式化的流程，减少了开发者在风格选择上的时间和精力投入，提高了团队协作效率，并有助于减少由格式问题引起的代码审查时间。'
+    },
   ]
 }
