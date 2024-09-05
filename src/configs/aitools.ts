da80510537c66811c6614af7abb1225a20e3577f
杨琼璞 <jeffygisgreat@gmail.com>
2024-09-05 23:22:07 +0800

    ✨ Update aitools config

--- src/configs/aitools.ts
+++ src/configs/aitools.ts
@@ -8,14 +8,9 @@ export default {
       desc: 'ChatGPT 帮助你获取答案、找到灵感并提高生产力。它无需登录，免费且易于使用。只需提问，ChatGPT 就能帮助你进行写作、学习、头脑风暴等。它是由 OpenAI 开发的高级对话模型服务，且在 MacOS 推出桌面应用，体验良好。'
     },
     {
-      title: 'Poe | 快捷实用的 AI 聊天',
-      link: 'https://poe.com/',
-      desc: 'Poe 是一个快速、有帮助的 AI 聊天网站。在这里，您可以免费与 ChatGPT、GPT-4、Genimi PRO、Claude 3 Opus、DALLE 3 以及其他数百万个 AI 进行对话。您可以选择使用 Google 账号、Apple 账号或者手机号登录。'
-    },
-    {
-      title: 'Meta AI ｜ Chat Wdith Llama 3',
-      link: 'https://llama3.dev/',
-      desc: 'Llama3 是 Meta AI 开发的高性能、多功能语言模型的最新版本。其主要亮点包括：扩展的知识库和功能、改进的多语言支持、增强推理和分析能力、持续致力于安全等；可在这个网站体验 Llama3:8B 及 Llama3:70B 等模型。'
+      title: 'OpenAI | 一家人工智能研究和部署公司',
+      link: 'https://openai.com/',
+      desc: 'OpenAI 是一家人工智能研究和部署公司。其使命是确保通用人工智能造福全人类；旗下推出 ChatGPT、GPT-4、Sora、DALL-E 等自然语言生成式模型，风靡全球（尤其是 ChatGPT）；OpenAI：引领人工智能革命的领军者。'
     },
     {
       title: 'Claude | Anthropic 的 AI 助理',
@@ -23,14 +18,29 @@ export default {
       desc: 'Claude 是由 Anthropic 开发的高级 AI 聊天机器人，旨在提供个人助理服务。用户可用自然语言与 Claude 互动，完成写作、学习和头脑风暴等任务。Claude 能处理多达 100K 个 token 的输入，适用于长篇文档和书籍。'
     },
     {
-      title: 'OpenAI | 一家人工智能研究和部署公司',
-      link: 'https://openai.com/',
-      desc: 'OpenAI 是一家人工智能研究和部署公司。其使命是确保通用人工智能造福全人类；旗下推出 ChatGPT、GPT-4、Sora、DALL-E 等自然语言生成式模型，风靡全球；OpenAI：引领人工智能革命的领军者。'
+      title: 'Meta AI ｜ Chat Wdith Llama 3',
+      link: 'https://llama3.dev/',
+      desc: 'Llama3 是 Meta AI 开发的高性能、多功能语言模型的最新版本。其主要亮点包括：扩展的知识库和功能、改进的多语言支持、增强推理和分析能力、持续致力于安全等；可在这个网站体验 Llama3:8B 及 Llama3:70B 等模型。'
+    },
+    {
+      title: '通义千问 | 你的全能AI助手',
+      link: 'https://tongyi.aliyun.com/',
+      desc: '通义千问是阿里云研发的大型语言模型，具备智能对话、文本创作等能力，支持多语言处理，适用于问答、聊天、写作等场景，强调合法合规，拒绝生成不当内容，提供高效、安全的服务，致力于理解和生成自然语言。'
+    },
+    {
+      title: 'Poe | 快捷实用的 AI 聊天',
+      link: 'https://poe.com/',
+      desc: 'Poe 是一个快速、有帮助的 AI 聊天网站。在这里，您可以免费与 ChatGPT、GPT-4、Genimi PRO、Claude 3 Opus、DALLE 3 以及其他数百万个 AI 进行对话。您可以选择使用 Google 账号、Apple 账号或者手机号登录。'
     },
     {
       title: 'Hugging Face | 人工智能社区构建未来',
       link: 'https://huggingface.co/',
       desc: 'Hugging Face 是一个面向自然语言处理（NLP）任务的开源平台和社区。它提供了丰富的工具和资源，使开发者能够轻松地构建、训练和部署自然语言处理模型。这使得在 NLP 领域进行协作和知识共享变得更加容易。'
+    },
+    {
+      title: 'Replicate | 使用 API 运行 AI',
+      link: 'https://replicate.com/',
+      desc: 'Replicate 是一个简化机器学习模型托管、运行和共享的平台。支持多种深度学习框架，提供简单API调用，便于模型推理和实验。拥有丰富的模型库，覆盖图像、文本等应用，促进团队协作。Replicate 使模型部署和使用高效直观，加速创新。'
     },
     {
       title: 'Ollama | 启动并运行大型语言模型',
