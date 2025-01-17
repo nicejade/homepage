afa7c948308d8554f8e4c0017595a320b2669d97
杨琼璞 <jeffygisgreat@gmail.com>
2025-01-17 13:30:29 +0800

    ✨ Update web dev tools config

--- src/configs/webDevTools.ts
+++ src/configs/webDevTools.ts
@@ -1,7 +1,17 @@
 export default {
-  title: '网站开发',
+  title: '网站优化',
   style: 'stagger',
   list: [
+    {
+      title: 'Google Search Console - 免费的 SEO 工具',
+      link: 'https://search.google.com/search-console/',
+      desc: 'Google Search Console 是一款免费的 SEO 工具，专为网站管理员和开发者设计，帮助监控和优化网站在 Google 搜索引擎中的表现。它提供全面的性能数据，包括关键词排名、点击率和展示量，帮助开发者了解用户如何通过搜索找到网站。'
+    },
+    {
+      title: 'Google Search Console - 富媒体搜索结果测试',
+      link: 'https://search.google.com/test/rich-results',
+      desc: 'Google Rich Results Test，专为开发者和站长设计，用于测试网页是否支持 Rich Results：指在 Google 搜索中呈现的增强显示形式，例如评分、商品价格、活动详情等）。通过该工具，开发者可以检测网页的结构化数据标记是否正确，并验证其兼容性。'
+    },
     {
       title: 'Seoggestion - 免费 SEO 元标签分析器与优化工具',
       link: 'https://www.seoggestion.com/',
@@ -10,7 +20,7 @@ export default {
     {
       title: 'MetaSEO | 实现搜索引擎优化的成功',
       link: 'https://metaseo.itsvg.in/',
-      desc: 'MetaSEO 是一款专为开发者设计的工具，旨在帮助优化网页的 meta 标签，从而提升搜索引擎优化（SEO）效果。通过使用 metaSEO，开发者可以轻松编辑和管理网页的标题（title）、描述（description）、关键词（keywords）等 meta 标签，确保网页在搜索引擎中获得更高的可见度。'
+      desc: 'MetaSEO 是一款专为开发者设计的工具，旨在帮助优化网页的 meta 标签，从而提升搜索引擎优化（SEO）效果。通过使用它，开发者可以轻松编辑和管理网页的标题（title）、描述、关键词等 meta 标签，确保网页在搜索引擎中获得更高的可见度。'
     },
     {
       title: 'Tabler Icons | 高度可定制的免费 SVG 图标',
