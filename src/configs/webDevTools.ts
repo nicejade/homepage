5a88b5abe4114bb21cdf01f78ac2d3cff58c9b48
杨琼璞 <jeffygisgreat@gmail.com>
2025-01-18 17:07:00 +0800

    ✨ Update links config

--- src/configs/webDevTools.ts
+++ src/configs/webDevTools.ts
@@ -13,6 +13,16 @@ export default {
       desc: 'Google Rich Results Test，专为开发者和站长设计，用于测试网页是否支持 Rich Results：指在 Google 搜索中呈现的增强显示形式，例如评分、商品价格、活动详情等）。通过该工具，开发者可以检测网页的结构化数据标记是否正确，并验证其兼容性。'
     },
     {
+      title: 'PWABuilder | 帮助开发者构建和发布 PWA',
+      link: 'https://www.pwabuilder.com/',
+      desc: 'PWABuilder 是微软推出的一款免费工具，旨在帮助开发者快速将网站转换为渐进式 Web 应用（PWA）。它支持一键生成 PWA 必需的配置文件（如 manifest 和 Service Worker），并提供多种模板，帮助开发者轻松实现离线支持、推送通知等功能。'
+    },
+    {
+      title: 'Sentry - 应用程序性能监控和错误跟踪软件',
+      link: 'https://github.com/getsentry/sentry',
+      desc: 'Sentry 是一款开源的实时错误监控和性能跟踪工具，专为开发者设计，用于快速识别、诊断和解决应用程序中的问题。支持前端、后端和移动端多种语言和框架，它能够自动捕获错误信息，包括堆栈跟踪、上下文数据和用户影响范围，帮助开发者精确定位问题根源。'
+    },
+    {
       title: 'Seoggestion - 免费 SEO 元标签分析器与优化工具',
       link: 'https://www.seoggestion.com/',
       desc: 'Seoggestion 是一款免费的 SEO 元标签分析和优化工具，旨在帮助开发者提升网页的搜索引擎优化效果。通过输入 HTTPS URL，Seoggestion 会对网页的元标签进行深入分析，提供优化建议，确保网页在搜索引擎中获得更高的可见度。'
