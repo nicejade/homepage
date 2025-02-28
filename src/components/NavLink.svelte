90296d432fa1d2358516812047c5c29966dea839
杨琼璞 <jeffygisgreat@gmail.com>
2025-02-28 13:59:48 +0800

    💄 Modify mockup image & desc

--- src/components/NavLink.svelte
+++ src/components/NavLink.svelte
@@ -5,7 +5,7 @@
 
 <a href={href} title={title} 
   class="nav-link py-2 px-5 me-3 mb-3 text-sm font-medium focus:outline-none bg-transparent rounded-full 
-  border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 md:px-4 md:py-1.5 md:me-2">
+  border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-0 md:px-4 md:py-1.5 md:me-2">
   {title}
 </a>
 
